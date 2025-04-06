68ddf554a5e817cec09fbee0bd8293866b322383
Kseniya <xeniyaoz45@gmail.com>
2025-04-06 21:53:01 +0300

    Add dynamic header (default-header) and sidebar.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Order;
+use App\Models\Task;
+use App\Models\TaskStatus;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +24,26 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        View::composer('layouts.sidebar', function ($view) {
+            $current_user = auth()->user();
+            $view->with('current_user', $current_user);
+        });
+
+        View::composer('layouts.default-header', function ($view) {
+            $current_user = auth()->user();
+            $in_process_status = TaskStatus::where('name', 'In Process')->first()->id;
+
+            $tasks_count =
+                Task::where('personal_id', $current_user->id)
+                    ->where('status_id', $in_process_status)->get()->count();
+            $orders_count =
+                Order::where('personal_id', $current_user->id)
+                    ->where('status_id', $in_process_status)->get()->count();
+
+            $view->with([
+                'tasks_count' => $tasks_count,
+                'orders_count' => $orders_count,
+            ]);
+        });
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,17 @@
 <div class="col-md-4 sidebar-container">
     <div class="text-break" id="personal_card" style="box-shadow: 0px 0px 4px;">
-        <div class="d-flex d-lg-flex flex-column align-items-center align-items-lg-center"><img id="personal-card-photo" src="/assets/img/young-smiling-girl-student-about-20-25-years-old-.jpg" style="box-shadow: 0px 0px 8px;"><span id="personal-card-company-name" style="text-align: center;font-size: 14px;color: var(--bs-gray);font-family: 'Arsenal SC', sans-serif;">Microsift</span><span id="personal-card-personal-name" style="text-align: center;font-size: 18px;color: var(--bs-dark);font-family: 'BIZ UDPGothic', sans-serif;font-weight: bold;">Jane Dow (Login)</span></div>
+        <div class="d-flex d-lg-flex flex-column align-items-center align-items-lg-center">
+            <img id="personal-card-photo" src="{{ asset('storage/'.$current_user->avatar) }}" style="box-shadow: 0px 0px 8px;">
+            <span id="personal-card-company-name" style="text-align: center;font-size: 14px;color: var(--bs-gray);font-family: 'Arsenal SC', sans-serif;">Microsift</span>
+            <span id="personal-card-personal-name" style="text-align: center;font-size: 18px;color: var(--bs-dark);font-family: 'BIZ UDPGothic', sans-serif;font-weight: bold;">{{ $current_user->name }} ({{ $current_user->email }})</span>
+        </div>
         <div id="personal-card-data">
-            <div class="text-break"><span style="font-size: 18px;font-family: Carlito, sans-serif;">Position:&nbsp;</span><span class="text-break" style="font-size: 18px;font-family: Carlito, sans-serif;font-weight: bold;">Manager</span></div>
-            <div class="text-break"><span style="font-size: 18px;font-family: Carlito, sans-serif;">Salary:&nbsp;</span><span class="text-break" style="font-size: 18px;font-family: Carlito, sans-serif;font-weight: bold;">Manager</span></div>
-            <div class="text-break"><span style="font-size: 18px;font-family: Carlito, sans-serif;">Access level:&nbsp;</span><span class="text-break" style="font-size: 18px;font-family: Carlito, sans-serif;font-weight: bold;">Manager</span></div><a class="btn btn-primary" role="button" style="width: 100%;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-pencil-fill">
+            <div class="text-break"><span style="font-size: 18px;font-family: Carlito, sans-serif;">Position: </span>
+                <span class="text-break" style="font-size: 18px;font-family: Carlito, sans-serif;font-weight: bold;">{{ $current_user->position->name }}</span></div>
+            <div class="text-break"><span style="font-size: 18px;font-family: Carlito, sans-serif;">Salary: </span>
+                <span class="text-break" style="font-size: 18px;font-family: Carlito, sans-serif;font-weight: bold;">{{ $current_user->salary }}</span></div>
+            <div class="text-break"><span style="font-size: 18px;font-family: Carlito, sans-serif;">Access level: </span>
+                <span class="text-break" style="font-size: 18px;font-family: Carlito, sans-serif;font-weight: bold;">{{ $current_user->accessLevel->name }}</span></div><a class="btn btn-primary" role="button" style="width: 100%;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-pencil-fill">
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"></path>
                 </svg></a>
         </div>
